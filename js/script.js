dac8f2b438abc7cbd9961b655d0ae1ed5e297465
danilkovalyov <65535063+danilkovalyov@users.noreply.github.com>
2020-05-18 11:46:56 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:true,
+        dots:true,
+        adaptiveHeight:true,
+        slidesToShow:3,
+        speed:500,
+        easing:'easy',
+        // infinite:false
+    });
+});
